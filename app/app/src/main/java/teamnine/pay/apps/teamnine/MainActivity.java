ccb26e195664fff83893006e358cd01dcb89691e
kenneth <kmbuthia47@gmail.com>
2016-11-18 21:46:52 +0000

    Added more interface for registration

--- app/app/src/main/java/teamnine/pay/apps/teamnine/MainActivity.java
+++ app/app/src/main/java/teamnine/pay/apps/teamnine/MainActivity.java
@@ -1,13 +1,39 @@
 package teamnine.pay.apps.teamnine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnRegister;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(this);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnRegister){
+            try{
+                Intent register = new Intent(this, Register.class);
+                startActivity(register);
+            }
+            catch(Exception r){
+                r.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/app/src/main/java/teamnine/pay/apps/teamnine/Register.java
@@ -0,0 +1,19 @@
+package teamnine.pay.apps.teamnine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kenneth on 11/18/16.
+ */
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+
+    }
+}
