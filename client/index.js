8586717e288193c68a23e8169ee37864b4a9e262
ktertikas <ktertikas@gmail.com>
2016-11-19 02:49:37 +0000

    Styling of page header and map

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+var location = [];
+
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
