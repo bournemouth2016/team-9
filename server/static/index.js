5effc17cc49232ede469c4f6b5ed66aa3f625243
ktertikas <ktertikas@gmail.com>
2016-11-19 07:15:03 +0000

    Web client data parsing all ok

--- server/static/index.js
+++ server/static/index.js
@@ -8,7 +8,7 @@ ws.onopen = function(){
 
 
 
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  var mymap = L.map('mapid').setView([50.74299711383331, -1.830132394788736], 13);
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
@@ -18,19 +18,22 @@ ws.onopen = function(){
 
   ws.onmessage = function(event) {
     data_json = JSON.parse(event.data)
+    //console.log(data_json)
     if(data_json.hasOwnProperty('results')){
-        for (i in data_json['results']){
-
+        //console.log(data_json['results'].length);
+        for (i=0; i< data_json['results'].length; i++){
               // console.log(data_json['results'][i]);
-              var marker = L.marker(data_json['results'][i]).addTo(mymap);
-              marker.bindPopup("<strong>Name: </strong>"  + " <br><strong>Telephone:</strong>").openPopup();
+              var marker = L.marker(data_json['results'][i]['coordinates']).addTo(mymap);
+              marker.bindPopup("<strong>Name: </strong>"  + data_json['results'][i]['owner'] + " <br><strong>Telephone:</strong> "+data_json['results'][i]['phone']).openPopup();
         }
     }
     else{
         var new_marker = L.marker(data_json['coordinates']).addTo(mymap);
+        console.log(data_json);     
+        new_marker.bindPopup("<strong>Name: </strong>"  + data_json['owner'] + " <br><strong>Telephone:</strong> "+data_json['phone']).openPopup();
     }
   // var value = JSON.parse(event.data);
-  // console.log(value)
+  //console.log(value)
   
 };
   // marker.on("click",function(event){
